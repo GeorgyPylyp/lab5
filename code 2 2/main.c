711bf5ab7b730569660cb3f6df69bcc394d2bca1
GeorgyPylyp <132196813+GeorgyPylyp@users.noreply.github.com>
2023-06-01 17:51:58 +0300

    Update main.c

--- code 2 2/main.c
+++ code 2 2/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 int main(void) {
-const int n, Low=-100, High=100;
+const int n, Low=-100, High=200;
 printf("Введіть розмір масиву = ");
 scanf("%d", &n);
 srand(time(0));
@@ -24,7 +24,11 @@ if (a==0)
 if( a[i]>0)
  a1*=a[i];
 }
-printf("\n номер мінімального елемента масиву= %d\t", min2);
-printf("\n Добуток елементів масиву до першого нульового елемента= %d\n", a1);
+printf("\n номер мінімального елемента масиву= %d\n", min2);
+if (a1 != 1) {
+printf("\n Добуток елементів масиву до першого нульового елемента: %d\n", a1);
+} else {
+ printf("\n У масиві немає елементів більше нуля.\n");
+} 
 return 0;
 }
